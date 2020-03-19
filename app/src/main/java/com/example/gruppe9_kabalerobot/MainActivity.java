4f90827a713820cafb3ee9495d187bc27ba18315
niko5297 <s185082@student.dtu.dk>
2020-03-19 16:39:58 +0100

    Added Android's newest CameraAPI called CameraX.
    The app now shows a preview from the back camera

--- app/src/main/java/com/example/gruppe9_kabalerobot/MainActivity.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.gruppe9_kabalerobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startCamera = findViewById(R.id.startCamera);
+
+        startCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == startCamera){
+            Intent i = new Intent(this, CameraViewActivity.class);
+            startActivity(i);
+        }
     }
 }
