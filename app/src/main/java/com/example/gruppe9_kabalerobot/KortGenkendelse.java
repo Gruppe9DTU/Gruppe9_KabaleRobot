440561e1a4687c2acb1d15d7cc4b7c5e66a1d983
s185114 <s185114@student.dtu.dk>
2020-03-25 11:15:15 +0100

    OpenCV

--- app/src/main/java/com/example/gruppe9_kabalerobot/KortGenkendelse.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/KortGenkendelse.java
@@ -2,8 +2,24 @@ package com.example.gruppe9_kabalerobot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.opencv.android.*;
+
 public class KortGenkendelse extends AppCompatActivity {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status) {
+                case LoaderCallbackInterface.SUCCESS:
+                    break;
+            }
+        }
 
+    };
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+    }
 
 }
