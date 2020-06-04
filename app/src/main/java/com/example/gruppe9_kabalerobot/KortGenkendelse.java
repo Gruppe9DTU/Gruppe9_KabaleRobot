9a567b3d47a6c6832837fd409bfbd38832370c76
s185114 <s185114@student.dtu.dk>
2020-06-04 13:39:36 +0200

    IT WOOOOORKS!!!

--- app/src/main/java/com/example/gruppe9_kabalerobot/KortGenkendelse.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/KortGenkendelse.java
@@ -1,5 +1,6 @@
 package com.example.gruppe9_kabalerobot;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,17 @@ import org.opencv.android.*;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class KortGenkendelse extends AppCompatActivity implements View.OnClickListener {
     Button button;
@@ -32,7 +39,7 @@ public class KortGenkendelse extends AppCompatActivity implements View.OnClickLi
         button.setOnClickListener(this);
 
         image = findViewById(R.id.image123);
-        image.setImageResource(R.drawable.test4);
+        image.setImageResource(R.drawable.e212);
 
         textView = findViewById(R.id.hello);
 
@@ -55,42 +62,66 @@ public class KortGenkendelse extends AppCompatActivity implements View.OnClickLi
     }
 
     public void run() throws IOException {
-        System.out.println("running camera");
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat matrix;
-        //capture.read(matrix);
-        matrix = Utils.loadResource(this, R.drawable.test4, CvType.CV_8UC4);
-        //ic.imwrite("test.png", matrix);
+        matrix = Utils.loadResource(this, R.drawable.e212);
         Mat gray = new Mat();
         Imgproc.cvtColor(matrix, gray, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.medianBlur(gray, gray, 5);
+        CascadeClassifier cascade_2 = initClassifier(R.raw.a15cascade);
+        CascadeClassifier cascade_5 = initClassifier(R.raw.cascade);
 
-        Mat hsv = new Mat();
-        Imgproc.cvtColor(matrix, hsv, Imgproc.COLOR_BGR2HSV);
-        Scalar lower_bound = new Scalar(187, 182, 188);
-        Scalar upper_bound = new Scalar(250, 251, 251);
-        Mat dst = new Mat();
-        Core.inRange(matrix, lower_bound, upper_bound, dst);
-        Mat detect = new Mat();
-        Core.bitwise_and(hsv, hsv, detect, dst);
+        MatOfRect c2 = new MatOfRect();
+        MatOfRect c5 = new MatOfRect();
+        cascade_2.detectMultiScale(matrix,c2);
+        cascade_5.detectMultiScale(matrix,c5);
 
-        Mat circles = new Mat();
-        Imgproc.HoughCircles(gray, circles, Imgproc.HOUGH_GRADIENT, 1.0, (double) gray.rows()/16, 100.0, 30.0, 1, 50);
-
-        for (int x=0; x < circles.cols(); x++) {
-            double[] c = circles.get(0, x);
-            Point center = new Point(Math.round(c[0]), Math.round(c[1]));
-            Imgproc.circle(detect, center, 1, new Scalar(187, 182, 188), 1, 2, 0);
-            int radius = (int) Math.round(c[2]);
-            Imgproc.circle(detect, center, radius, new Scalar(250, 251, 251), 3, 8, 0);
+        for (Rect rect : c2.toArray()) {
+            Imgproc.rectangle(matrix, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(255, 0, 0));
+        }
+        for (Rect rect : c5.toArray()) {
+            Imgproc.rectangle(matrix, new Point(rect.x, rect.y),
+                    new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(0, 255, 0));
         }
 
-
-        Bitmap ne=Bitmap.createBitmap(matrix.width(),matrix.height(), Bitmap.Config.ARGB_8888);
-        Utils.matToBitmap(detect,ne);
+        Imgproc.cvtColor(matrix, matrix, Imgproc.COLOR_BGR2RGB);
+        Bitmap ne=Bitmap.createBitmap(matrix.width(),matrix.height(), Bitmap.Config.RGB_565);
+        Utils.matToBitmap(matrix,ne);
         image.setImageBitmap(ne);
 
 
+    }
+
+
+    CascadeClassifier initClassifier(int res){
+        CascadeClassifier cascade=null;
+        try {
+            InputStream is = getResources().openRawResource(res);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File mCascadeFile = new File(cascadeDir, "haarcascade_mcs_nose.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            cascade = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if (cascade.empty()) {
+                System.out.println("feck");
+                cascade = null;
+            } else
+                System.out.println("yay");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("oh no");
+        }
+        return cascade;
     }
 
 }
