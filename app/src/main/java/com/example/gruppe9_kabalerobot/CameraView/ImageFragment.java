a7ec7677102bc0715cc46387ebc6b3bc762cda96
s185114 <s185114@student.dtu.dk>
2020-06-04 14:17:13 +0200

    purged imports

--- app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/ImageFragment.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/ImageFragment.java
@@ -1,6 +1,5 @@
 package com.example.gruppe9_kabalerobot.CameraView;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,26 +9,10 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.gruppe9_kabalerobot.Haarcascade.Haarcascade;
 import com.example.gruppe9_kabalerobot.R;
-
-import org.opencv.android.Utils;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfRect;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.CascadeClassifier;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class ImageFragment extends Fragment {
 
