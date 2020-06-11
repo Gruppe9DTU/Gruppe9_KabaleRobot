5149803d6d52112ce7ecc5cfbb72693cee09417f
Anders <s185100@student.dtu.dk>
2020-06-11 10:07:37 +0200

    AndroidManifest updated with internet permissions and made Server.java a singleton.

--- app/src/main/java/com/example/gruppe9_kabalerobot/Socket/Server.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/Socket/Server.java
@@ -1,4 +1,18 @@
 package com.example.gruppe9_kabalerobot.Socket;
 
 public class Server {
+
+    final int SERVERPORT = 6000;
+
+    private static Server instance;
+
+    private Server(){}
+
+    public static synchronized Server getInstance(){
+        if(instance == null){
+            instance = new Server();
+        }
+        return instance;
+    }
+
 }
