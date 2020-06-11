a1b2eaa427b3c55e914f937f56eabe7bd7b1d38b
Kristian Andersen <s185125@student.dtu.dk>
2020-06-11 11:26:52 +0200

    Minor text changes

--- app/src/test/java/com/example/gruppe9_kabalerobot/CardPlacement/CardTranslatorTest.java
+++ app/src/test/java/com/example/gruppe9_kabalerobot/CardPlacement/CardTranslatorTest.java
@@ -1,27 +1,37 @@
 package com.example.gruppe9_kabalerobot.CardPlacement;
 
+import com.example.gruppe9_kabalerobot.Framework.controller.GameLogic;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class CardTranslatorTest {
     private CardPlacement placement;
-
+    private CardTranslator translator;
+    private GameLogic game;
 
     @Before
     public void setup() {
-
+        this.placement = new CardPlacement();
+        this.translator = new CardTranslator(placement);
     }
 
     /**
-     * Only Waste
+     * Only Waste are filled
      */
     @Test
     public void insertCards101() {
+        //Insert cards into CardPlacement
+        //Setup expected Lists
 
+        //Translate cards
+        translator.insertCards(game);
+
+        //AssertEqual expected Lists by reading from game
     }
 
     /**
-     * Only Foundation
+     * Only Foundation are filled
      */
     @Test
     public void insertCards102() {
@@ -29,7 +39,7 @@ public class CardTranslatorTest {
     }
 
     /**
-     * Only Tableau
+     * Only Tableaus are filled
      */
     @Test
     public void insertCards103() {
@@ -37,7 +47,7 @@ public class CardTranslatorTest {
     }
 
     /**
-     * All
+     * All are filled
      */
     @Test
     public void insertCards104() {
