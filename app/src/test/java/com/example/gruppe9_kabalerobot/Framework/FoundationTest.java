637e34849cc1a17de778bd3733bcb23039758dd6
Anders <s185100@student.dtu.dk>
2020-06-11 15:44:13 +0200

    Merge with Framework branch since app won't run and the Framework branch works.
    Squashed commit of the following:
    
    commit e72a949e56f8aba404bf64e646815c2260f40fa3
    Merge: 51ee5a0 c28c6e7
    Author: kna1711 <43743698+kna1711@users.noreply.github.com>
    Date:   Wed Jun 10 15:31:44 2020 +0200
    
        Merge pull request #10 from Gruppe9DTU/MoveOrderFix
    
        Changed foundationToTableau to lower priority, added moveTableau to m…
    
    commit c28c6e7a35b60347777e0fd92a615dcfcb5fa149
    Author: Anders <s185100@student.dtu.dk>
    Date:   Wed Jun 10 15:05:49 2020 +0200
    
        Changed foundationToTableau to lower priority, added moveTableau to moveChecker and corrected the getBestMove tests.
    
    commit 51ee5a00d33e34d59593cd0693e7439545d05a59
    Author: Kristian Andersen <s185125@student.dtu.dk>
    Date:   Wed Jun 10 14:49:20 2020 +0200
    
        Framework classes and test class MoveAlgoritmTest have been copy-pasted into this project, from previoues framework project (https://github.com/Gruppe9DTU/7KabaleFramework)
    
    commit dca38506b90b6b22d4d4677d7b3805715b884eb3
    Author: niko5297 <s185082@student.dtu.dk>
    Date:   Tue Jun 9 10:45:56 2020 +0200
    
        Added Framework classes and Frameworks tests

---
+++ app/src/test/java/com/example/gruppe9_kabalerobot/Framework/FoundationTest.java
@@ -0,0 +1,54 @@
+package com.example.gruppe9_kabalerobot.Framework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FoundationTest {
+
+    @Test
+    public void testAddCard() {
+        Foundation testFoundation = new Foundation();
+        Card testCard1 = new Card(0, 1);
+        Card testCard2 = new Card(0, 2);
+        Card testCard3 = new Card(1, 2);
+        Card testCard4 = new Card(0, 3);
+        testFoundation.addCard(testCard2);
+        assertEquals(0, testFoundation.countCards());
+        testFoundation.addCard(testCard1);
+        assertEquals(1, testFoundation.countCards());
+        assertEquals(testCard1.toString(), testFoundation.peekCard().toString());
+        testFoundation.addCard(testCard3);
+        assertEquals(1, testFoundation.countCards());
+        assertEquals(testCard1.toString(), testFoundation.peekCard().toString());
+        testFoundation.addCard(testCard4);
+        assertEquals(1, testFoundation.countCards());
+        assertEquals(testCard1.toString(), testFoundation.peekCard().toString());
+        testFoundation.addCard(testCard2);
+        assertEquals(2, testFoundation.countCards());
+        assertEquals(testCard2.toString(), testFoundation.peekCard().toString());
+        testFoundation.addCard(testCard4);
+        assertEquals(3, testFoundation.countCards());
+        assertEquals(testCard4.toString(), testFoundation.peekCard().toString());
+
+    }
+
+    @Test
+    public void testTakeCard() {
+        Foundation testFoundation = new Foundation();
+        Card testCard1 = new Card(0, 1);
+        testFoundation.addCard(testCard1);
+        assertEquals(testCard1.toString(), testFoundation.takeCard().toString());
+    }
+
+    @Test
+    public void testIsComplete() {
+        Foundation testFoundation = new Foundation();
+        Card[] cards = new Card[13];
+        for( int i = 0; i < 13; i++)
+            cards[i] = new Card(0, i+1);
+        for( Card c : cards )
+            testFoundation.addCard(c);
+        assertTrue(testFoundation.isComplete());
+    }
+}
