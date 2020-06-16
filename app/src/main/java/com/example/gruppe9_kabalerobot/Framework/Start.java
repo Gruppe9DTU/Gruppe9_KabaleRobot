3009555ceb87c349c5d1ca725c54f9334684e5e8
niko5297 <s185082@student.dtu.dk>
2020-06-16 09:51:57 +0200

    Correction after merge

--- app/src/main/java/com/example/gruppe9_kabalerobot/Framework/Start.java
+++
@@ -1,9 +0,0 @@
-package com.example.gruppe9_kabalerobot.Framework;
-
-public class Start {
-    public static void main(String[] args) {
-//        Deck deck = new Deck();
-        PreviousStatesContainer previousStatesContainer = new PreviousStatesContainer();
-        GameLogic gamelogic = new GameLogic();
-    }
-}
