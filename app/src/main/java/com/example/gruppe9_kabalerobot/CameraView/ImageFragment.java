78e54d530808ec2307e569f73b022053af43c1c2
niko5297 <s185082@student.dtu.dk>
2020-06-17 10:48:20 +0200

    ImageFragment.java
    - Added null check to see if we recieve empty data
    
    Client.java
    
    - Added docs and comments

--- app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/ImageFragment.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/ImageFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -120,6 +121,10 @@ public class ImageFragment extends Fragment {
 
             dataArray = c.recieveData();
 
+            if (dataArray==null){
+                cancel(true);
+            }
+
             rectanglesDrawn = openCV.drawRectangles(bitmap,dataArray);
 
             //TODO: Uncomment when correct data is available
@@ -135,6 +140,15 @@ public class ImageFragment extends Fragment {
             */
 
             return null;
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+
+            getActivity().runOnUiThread(() -> {
+                Toast.makeText(getActivity(), "Fandt ingen kort. Tag et nyt billede og prøv igen..", Toast.LENGTH_LONG).show();
+            });
         }
 
         @Override
