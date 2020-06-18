191bca866b667d21f60891700cb8f02f8d6c7e4e
Kristian Andersen <s185125@student.dtu.dk>
2020-06-18 10:15:52 +0200

    Gonna be a few commits that doesn't do much, since I'm testing on the side

---
+++ app/src/test/java/com/example/gruppe9_kabalerobot/Framework/controller/SolitaireControllerTest.java
@@ -0,0 +1,22 @@
+package com.example.gruppe9_kabalerobot.Framework.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolitaireControllerTest {
+
+    /**
+     * Tests to see if it remembers a previous move suggestion that has been saved to the PreviousStateContainer
+     * Essentially a test if the Singleton works
+     */
+    @Test
+    public void takeMove102() {
+        //Setup CardPlacement with a possible move
+
+        //Setup Rest of the classes
+            //CardTranslator
+
+    }
+}
