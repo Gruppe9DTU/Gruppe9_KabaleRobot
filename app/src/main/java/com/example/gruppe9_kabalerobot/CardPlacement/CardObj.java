2861e41ca6cdcb2a2c521a6cbccd346337d2bb4f
Kristian Andersen <s185125@student.dtu.dk>
2020-06-22 15:26:24 +0200

    Looking through and writing comments to classes Comments for CardObj Deck class removed and test it was in has been rewritten

--- app/src/main/java/com/example/gruppe9_kabalerobot/CardPlacement/CardObj.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/CardPlacement/CardObj.java
@@ -1,8 +1,11 @@
 package com.example.gruppe9_kabalerobot.CardPlacement;
 
+/**
+ * Class to act as an Card, with the information of where it is on a matrix
+ */
 public class CardObj {
-
     private int x, y, value, suit;
+
     public CardObj(int x, int y, int value, int suit){
         this.x = x;
         this.y = y;
@@ -10,23 +13,23 @@ public class CardObj {
         this. value = value;
     }
 
+    /**
+     * Getters and setters
+     */
     public int getX() {
         return x;
+    }
+    public void setX(int x) {
+        this.x = x;
     }
 
     public int getY() {
         return y;
     }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public void setY(int y) {
         this.y = y;
     }
 
     public int getSuit() { return suit; }
-
     public int getValue() { return value; }
 }
