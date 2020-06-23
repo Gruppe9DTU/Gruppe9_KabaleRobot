2fed36adfc27c2d0f9f093396660d93332e14d81
niko5297 <s185082@student.dtu.dk>
2020-06-23 09:14:33 +0200

    EditPlacementFragment.java
    - Added edittext instead cause of time savings
    
    drawable
    
    - Added comfirm icon
    
    layout
    
    - Changed layout to have edittext instead of buttons
    
    strings
    
    - Added intro text for editing placement

--- app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/EditPlacementFragment.java
+++ app/src/main/java/com/example/gruppe9_kabalerobot/CameraView/EditPlacementFragment.java
@@ -23,14 +23,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditPlacementFragment extends Fragment implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
-    private Button waste, foundation1, foundation2, foundation3, foundation4;
-    private Button tab1For, tab2For, tab3For, tab4For, tab5For, tab6For, tab7For;
-    private Button tab1Bag, tab2Bag, tab3Bag, tab4Bag, tab5Bag, tab6Bag, tab7Bag;
+    private EditText waste, foundation1, foundation2, foundation3, foundation4;
+    private EditText tab1For, tab2For, tab3For, tab4For, tab5For, tab6For, tab7For;
+    private EditText tab1Bag, tab2Bag, tab3Bag, tab4Bag, tab5Bag, tab6Bag, tab7Bag;
     private EditText hiddencardsTab1,hiddencardsTab2,hiddencardsTab3,
             hiddencardsTab4,hiddencardsTab5,hiddencardsTab6,hiddencardsTab7;
     private ToggleButton wasteToggle;
     private FloatingActionButton done;
-    private Spinner typeSpinner, valueSpinner;
 
 
 
@@ -75,25 +74,6 @@ public class EditPlacementFragment extends Fragment implements CompoundButton.On
         done                = view.findViewById(R.id.done);
 
         wasteToggle.setOnCheckedChangeListener(this);
-        waste.setOnClickListener(this);
-        foundation1.setOnClickListener(this);
-        foundation2.setOnClickListener(this);
-        foundation3.setOnClickListener(this);
-        foundation4.setOnClickListener(this);
-        tab1For.setOnClickListener(this);
-        tab2For.setOnClickListener(this);
-        tab3For.setOnClickListener(this);
-        tab4For.setOnClickListener(this);
-        tab5For.setOnClickListener(this);
-        tab6For.setOnClickListener(this);
-        tab7For.setOnClickListener(this);
-        tab1Bag.setOnClickListener(this);
-        tab2Bag.setOnClickListener(this);
-        tab3Bag.setOnClickListener(this);
-        tab4Bag.setOnClickListener(this);
-        tab5Bag.setOnClickListener(this);
-        tab6Bag.setOnClickListener(this);
-        tab7Bag.setOnClickListener(this);
         done.setOnClickListener(this);
 
 
@@ -107,58 +87,23 @@ public class EditPlacementFragment extends Fragment implements CompoundButton.On
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
+
+
     }
 
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
+        if (view == done){
 
-            case R.id.wasteCard:
-                //Toast.makeText(getActivity(), "Test", Toast.LENGTH_SHORT).show();
-                 //buildDialog(); break;
 
-            case R.id.foundation1:
-
-            case R.id.foundation2:
-
-            case R.id.foundation3:
-
-            case R.id.foundation4:
-
-            case R.id.tab1F:
-
-            case R.id.tab2F:
-
-            case R.id.tab3F:
-
-            case R.id.tab4F:
-
-            case R.id.tab5F:
-
-            case R.id.tab6F:
-
-            case R.id.tab7F:
-
-            case R.id.tab1B:
-
-            case R.id.tab2B:
-
-            case R.id.tab3B:
-
-            case R.id.tab4B:
-
-            case R.id.tab5B:
-
-            case R.id.tab6B:
-
-            case R.id.tab7B:
-
-            case R.id.done:
 
         }
 
     }
+
+
+
 
 /*
     private void buildDialog() {
